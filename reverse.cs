dcf80d25f4c5b6e54dfb9fe2250f3ceec6de6f31
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-20 19:41:07 +0545

    Add files via upload
    How to reverse a string using a C# program

---
+++ reverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        // Convert to char array, reverse it, and create new string
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        string reversed = new string(charArray);
+
+        
+        Console.WriteLine("Reversed string: " + reversed);
+    }
+}
