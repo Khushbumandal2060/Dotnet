bd928db13491ed601cf83d09059049657a4ed33b
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-20 19:57:54 +0545

    Add files via upload
     a program in C# to convert a string array to a string.

---
+++ string.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        string[] words = { "Hello", "World", "from", "C#" };
+
+    
+        string result = string.Join(" ", words);
+
+        
+        Console.WriteLine("Combined string: " + result);
+    }
+}
