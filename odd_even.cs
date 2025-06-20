6833187ba69fb0f88bc390a51655e1688e6f3e0b
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-20 19:43:20 +0545

    Add files via upload
     Program to input number from user and find odd even accordingly.

---
+++ odd_even.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        // Check if number is even or odd
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"{number} is Even.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is Odd.");
+        }
+    }
+}
