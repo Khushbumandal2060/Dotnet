d912f219b270355d5a2f412c6fa78062eeece1f6
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-20 20:44:10 +0545

    Add files via upload
    Program to demonstrate partial class.

---
+++ StudentPart1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public partial class Student
+{
+    public string Name;
+    public int Age;
+
+    public void DisplayBasicInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Age: {Age}");
+    }
+}
