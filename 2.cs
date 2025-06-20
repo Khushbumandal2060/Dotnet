a1e26e9022af7000322853d5da3065d1fd165891
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-20 19:27:56 +0545

    Add files via upload
     program that obtains three integer values from the user and display the 
    sum.

---
+++ 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Enter first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        
+        Console.Write("Enter second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        
+        Console.Write("Enter third number: ");
+        int num3 = int.Parse(Console.ReadLine());
+
+        
+        int sum = num1 + num2 + num3;
+
+        
+        Console.WriteLine("The sum of the three numbers is: " + sum);
+    }
+}
