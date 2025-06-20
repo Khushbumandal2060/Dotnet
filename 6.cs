dda7f28b016b8b3a0e453dead8abb6be94c5c1b2
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-20 19:36:07 +0545

    Add files via upload
     program to swap two numbers entered by users.

---
+++ 6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Enter first number (a): ");
+        int a = int.Parse(Console.ReadLine());
+
+        
+        Console.Write("Enter second number (b): ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nBefore swapping: a = {a}, b = {b}");
+
+        // Swapping using a temporary variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine($"After swapping: a = {a}, b = {b}");
+    }
+}
