9224a0657f9b2f571675b2c4cff207cd68b6cc9c
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-20 19:34:14 +0545

    Add files via upload
     program to print the result of the specified operations- Take two input 
    number and one operator from user.

---
+++ 5.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Take first number
+        Console.Write("Enter first number: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        // Take second number
+        Console.Write("Enter second number: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        // Take operator (+, -, *, /)
+        Console.Write("Enter operator (+, -, *, /): ");
+        char op = Convert.ToChar(Console.ReadLine());
+
+        double result;
+
+        // Perform operation based on operator
+        switch (op)
+        {
+            case '+':
+                result = num1 + num2;
+                Console.WriteLine($"Result: {num1} + {num2} = {result}");
+                break;
+            case '-':
+                result = num1 - num2;
+                Console.WriteLine($"Result: {num1} - {num2} = {result}");
+                break;
+            case '*':
+                result = num1 * num2;
+                Console.WriteLine($"Result: {num1} * {num2} = {result}");
+                break;
+            case '/':
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    Console.WriteLine($"Result: {num1} / {num2} = {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: Cannot divide by zero.");
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid operator.");
+                break;
+        }
+    }
+}
