716c82f05c797cdb0d59a7fa0cb1a0c59b78a39d
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-20 20:34:20 +0545

    Add files via upload
     Program to access class method and fields of different namespace.

---
+++ person.cs
@@ -0,0 +1,16 @@
+﻿namespace MyApplication.Models
+{
+    public class Person
+    {
+        // Fields
+        public string Name;
+        public int Age;
+
+        // Method
+        public void Display()
+        {
+            System.Console.WriteLine($"Name: {Name}");
+            System.Console.WriteLine($"Age: {Age}");
+        }
+    }
+}
