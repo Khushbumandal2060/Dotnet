0e9b0207a0349077e428eaef3f2808eb17a00146
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-20 20:03:03 +0545

    Add files via upload
     Program to create employee class with name, email and salary field, also 
    create object for employee.

---
+++ employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Employee
+{
+   
+    public string Name;
+    public string Email;
+    public double Salary;
+
+    // Constructor
+    public Employee(string name, string email, double salary)
+    {
+        Name = name;
+        Email = email;
+        Salary = salary;
+    }
+
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Email: {Email}");
+        Console.WriteLine($"Salary: {Salary:C}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee emp = new Employee("Khushbu Mandal","khushbumandal225@gmail.com", 50000);
+
+        
+        emp.Display();
+    }
+}
