e319de857d68258b3906877825faef810d49675b
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-20 19:10:16 +0545

    Add files via upload
     program take input from user to print Yourname and print

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ask user to enter their name
+        Console.Write("Enter your name: ");
+        string yourName = Console.ReadLine();
+
+        // Print the welcome message
+        Console.WriteLine("Welcome " + yourName + ", to dotnet technology.");
+    }
+}
