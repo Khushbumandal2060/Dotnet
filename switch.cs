96832a86c6427e64c0e538b259aa8ff80aca6348
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-20 19:45:41 +0545

    Add files via upload
     Program to check whether an alphabet is vowel or consonant using switch 
    statement.

---
+++ switch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Input a single character
+        Console.Write("Enter an alphabet: ");
+        char ch = char.ToLower(Console.ReadKey().KeyChar);  // Accept char and convert to lowercase
+        Console.WriteLine();
+
+      
+        switch (ch)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                Console.WriteLine($"{ch} is a vowel.");
+                break;
+            default:
+                if (char.IsLetter(ch))
+                    Console.WriteLine($"{ch} is a consonant.");
+                else
+                    Console.WriteLine("Invalid input. Please enter an alphabet.");
+                break;
+        }
+    }
+}
