895e2917595e5d99be5db2f643f351ad45cc487e
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-22 00:10:53 +0545

    Add files via upload
     A class named ‘Shape’ having Length and Breadth properties along with default and 
    parametrized constructor. The parametrized constructor  accept two inputs and initialize 
    the properties of the class. Creating new class rectangle from shape class and calculating area of 
    rectangle.

---
+++ rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Shape
+{
+    // Properties
+    public double Length { get; set; }
+    public double Breadth { get; set; }
+
+    // Default Constructor
+    public Shape()
+    {
+        Length = 0;
+        Breadth = 0;
+        Console.WriteLine("Default constructor of Shape called.");
+    }
+
+    // Parameterized Constructor
+    public Shape(double length, double breadth)
+    {
+        Length = length;
+        Breadth = breadth;
+        Console.WriteLine("Parameterized constructor of Shape called.");
+    }
+}
+
+// Derived class: Rectangle
+class Rectangle : Shape
+{
+    // Constructor that calls base class constructor
+    public Rectangle(double length, double breadth) : base(length, breadth)
+    {
+    }
+
+    public double GetArea()
+    {
+        return Length * Breadth;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create Rectangle object using parameterized constructor
+        Rectangle rect = new Rectangle(10, 5);
+
+
+        Console.WriteLine($"Area of rectangle: {rect.GetArea()}");
+    }
+}
