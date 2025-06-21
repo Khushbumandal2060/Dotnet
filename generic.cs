7f2c6721d6717396049ef4df149e5e6f7ab2481b
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-22 00:13:34 +0545

    Add files via upload
    Program to create generic class and methods.

---
+++ generic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Generic class
+class MyGenericClass<T>
+{
+    private T value;
+
+    // Constructor
+    public MyGenericClass(T val)
+    {
+        value = val;
+    }
+
+    // Generic method inside generic class
+    public void Display()
+    {
+        Console.WriteLine("Value: " + value);
+    }
+
+    // Separate generic method
+    public void ShowType<U>(U data)
+    {
+        Console.WriteLine("Generic method value: " + data);
+        Console.WriteLine("Type of U is: " + data.GetType());
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create generic class object with int type
+        MyGenericClass<int> obj1 = new MyGenericClass<int>(100);
+        obj1.Display();
+        obj1.ShowType<string>("Hello");
+
+        // Create generic class object with string type
+        MyGenericClass<string> obj2 = new MyGenericClass<string>("C# Generics");
+        obj2.Display();
+        obj2.ShowType<double>(3.14);
+    }
+}
