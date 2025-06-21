78dbc6a034c8a04a57607688acf8c55116e524aa
Khushbu  Kumari Mandal <112657238+Khushbumandal2060@users.noreply.github.com>
2025-06-21 23:59:58 +0545

    Add files via upload
     A program in C# to overload unary operator.

---
+++ unary_operator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Number
+{
+    public int Value;
+
+    // Constructor
+    public Number(int value)
+    {
+        Value = value;
+    }
+
+    // Overload unary minus operator
+    public static Number operator -(Number n)
+    {
+        return new Number(-n.Value);
+    }
+
+    // Method to display value
+    public void Display()
+    {
+        Console.WriteLine($"Value: {Value}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Number num = new Number(100);
+        Console.WriteLine("Original number:");
+        num.Display();
+
+        Number negatedNum = -num;
+        Console.WriteLine("After overloading unary - operator:");
+        negatedNum.Display();
+    }
+}
